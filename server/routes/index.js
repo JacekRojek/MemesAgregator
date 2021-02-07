28e84f59fa09b2758c7bf6fe7ec4a27778643b97
Jacek <j.rojek19@gmail.com>
2021-02-08 05:41:32 +1100

    get next image route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,30 +4,45 @@ const { getImages } = require('../sources/jbzd');
 
 const router = express.Router();
 const REDIS_KEYS = {
-  JBZD: 'JBZD',
+  JBZD: 'jbzd:',
 };
 
 const url = 'https://jbzd.com.pl/str/2';
 
+router.get('/refresh', async (req, res, next) => {
+  const memes = await getImages(url);
+  memes.forEach((m, i) => {
+    client.set(`${REDIS_KEYS.JBZD}${i}`, m.src);
+  });
+  res.json({ ok: 'true' });
+});
+
 /* GET home page. */
-router.get('/next', async (req, res, next) => {
-  client.get(`${REDIS_KEYS}_1`, (err, result) => {
+router.get('/:key/:id', async (req, res, next) => {
+  const { params } = req;
+  client.get(params.id, (err, result) => {
     if (err) {
       console.error(err);
       res.json({ error: 'error' });
     } else {
-      res.redirect(result);
+      res.json(result);
     }
   });
 });
 
-router.get('/refresh', async (req, res, next) => {
-  const memes = await getImages(url);
-  memes.forEach((m, i) => {
-    console.log(`${REDIS_KEYS}_${i}: `, m.src);
-    client.set(`${REDIS_KEYS}_${i}`, m.src);
+router.get('/:key/', async (req, res, next) => {
+  const { params } = req;
+  const data = [];
+  const stream = client.scanStream({
+    match: `${REDIS_KEYS.JBZD}*`,
+    count: 100,
   });
-  res.json({ ok: 'true' });
+  stream.on('data', (keys) => {
+    data.push([...keys]);
+  })
+  stream.on('end', () => {
+    res.json({ data });
+  })
 });
 
 module.exports = router;
